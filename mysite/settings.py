fe1b88a0e0c7c1d4c8ecd1d15a0656b4a0fc1568
mamaredo_d <11kakeru30@gmail.com>
2022-09-10 16:11:44 +0900

    ignoreの追加

--- mysite/settings.py
+++ mysite/settings.py
@@ -20,7 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-7@(f0j$wdi@y(k745^x4pl&v^al377zdkbi5(pk%1=tzb^7c29'
+try:
+    from .local_settings import *
+except ImportError:
+    pass
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
